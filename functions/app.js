bc4e2776125c24ff79eb001fe0e8445266a1d3db
Benjamin Kostiuk <benjamin.kostiuk@peelregion.ca>
2019-08-27 12:17:56 -0400

    clean up comments and code

--- functions/app.js
+++ functions/app.js
@@ -8,15 +8,16 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-// Import cloud function for Dialogflow Webhook
+// Import Dialogflow app for Google Assistant for local debugging
 const googleAssistant = require('./index.js').googleAssistantAgent;
 
 // Create express app
 let app = express().use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('online')); // Test route to see if app is running (Look at this in a browser)
+
 /*
-    Send the dialoflow request through this route.
+    Send the dialoflow request through the /dialogflow route.
     Remember to set your Webhook URL to [ngrok url]/dialogflow when developping locally
 */
 app.post('/dialogflow', googleAssistant);
